1b251da0eac92109066d956835714abb553253b1
bugsfunny <js.denize@gmail.com>
2023-09-18 11:30:23 +0200

    Add some test

--- projects/password-generator/src/app/app.component.spec.ts
+++ projects/password-generator/src/app/app.component.spec.ts
@@ -1,7 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { Spectator, createComponentFactory } from '@ngneat/spectator';
 
-describe('AppComponent', () => {
+describe('AppComponent avec TestBed', () => {
   let fixture: ComponentFixture<AppComponent>;
   let component: AppComponent;
   beforeEach(() => {
@@ -66,6 +67,38 @@ describe('AppComponent', () => {
     lengthInput.value = '33';
     lengthInput.dispatchEvent(new Event('input'));
 
+    expect(component.length).toBe(33);
+  });
+});
+
+describe('AppComponent avec Spectator', () => {
+  let spectator: Spectator<AppComponent>;
+  let component: AppComponent;
+  const createComponent = createComponentFactory({
+    component: AppComponent,
+    declarations: [AppComponent],
+  });
+  beforeEach(() => {
+    spectator = createComponent();
+    component = spectator.component;
+  });
+  it('should work', () => {
+    const password = spectator.query('article');
+    expect(password).toHaveText('Cliquez sur le bouton pour "Générer"');
+  });
+  it('should change message', () => {
+    spectator.click('button');
+    const password = spectator.query('article');
+    expect(password).toHaveText('Un mot de passe super fort!!!');
+  });
+  it('should change props', () => {
+    spectator.click('#uppercase');
+    expect(component.uppercase).toBeTrue();
+    spectator.click('#numbers');
+    expect(component.numbers).toBeTrue();
+    spectator.click('#symbols');
+    expect(component.symbols).toBeTrue();
+    spectator.typeInElement('33', '#length');
     expect(component.length).toBe(33);
   });
 });
